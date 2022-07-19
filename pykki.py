8a52f5eafc3811236e925621cc0666816eacabbc
xakv <zachvatwork@gmail.com>
2022-07-18 22:49:10 -0400

    a class

--- pykki.py
+++ pykki.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 from pathlib import Path
+from unicodedata import name
 import typer
 import rich
 import configparser
 import os
 import sys
 
-Class TUI_Cheat():
-    
+
+class TUI_Cheat:
+    def __init__(self):
+        self.name = name
+        self.ref_path = Path.home() / '.config' / 'pykki' / self.name
+        self.tags = []
+        self.src_url = src_url
+
 
 def 
